e0e1a3888037d374a220934c5256a348c5f2e759
SarvaniKarri1234 <155243027+SarvaniKarri1234@users.noreply.github.com>
2023-12-31 15:37:09 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('pedestrian detector')
+st.header('Testing streamlit')
+st.text('pedestrian found')
+st.text_input('first name')
+st.number_input('pick a number',0,10)
